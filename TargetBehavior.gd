86ab9da94b60b0dff88b235486dca71035d6c81f
Sid Spry <github@sid.aeam.us>
2020-12-18 17:27:47 -0600

    Target collision

--- TargetBehavior.gd
+++ TargetBehavior.gd
@@ -2,8 +2,15 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	connect('body_entered', self, '_on_body_entered')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _integrate_forces(state):
+	#var b = get_colliding_bodies()
+	pass
+
+func _on_body_entered(body):
+	print(body)
